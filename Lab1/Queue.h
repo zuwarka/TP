c7a84f0ed51c6e947b04aefd77416c3f501e9bc1
zuwarka <76390605+zuwarka@users.noreply.github.com>
2021-02-05 18:46:58 +0300

    Create Queue.h

---
+++ Lab1/Queue.h
@@ -0,0 +1,33 @@
+#pragma once
+//using namespace std;
+
+typedef struct Unit //элемент очереди
+{
+	int data = 0; //значение
+	Unit* prev = 0; //указатель на предыдущи элемент
+} Unit;
+
+class parQueue
+{
+private:
+	Unit* last = 0; //указатель на "конец" очереди
+
+protected:
+	int size; //размер очереди
+	//Unit* get_last(); //объявление функции взятия конца очереди
+	//int get_size(); //объявление функции взятия размера очереди
+
+public:
+	parQueue(); //конструктор без параметров
+	~parQueue(); //деструктор
+
+	Unit* get_last(); //объявление функции взятия конца очереди
+
+	int pop(); //удаление элемента очереди
+	void push(int el); //добавление элемента очереди
+	void print(); //вывод на консоль очереди
+	void merge(parQueue& Q1, parQueue& Q2); //слияние двух очередей
+	void copy(parQueue& Q); //копирование очереди
+	bool isEmpty(); //проверка на заполненность
+
+};
