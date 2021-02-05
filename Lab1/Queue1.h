b87adb240587da0be695c49ec8701c1875e5ad0f
zuwarka <76390605+zuwarka@users.noreply.github.com>
2021-02-05 18:48:00 +0300

    Create Queue1.h

---
+++ Lab1/Queue1.h
@@ -0,0 +1,32 @@
+#pragma once
+#include "Queue.h"
+
+class sonQueue_private : private parQueue
+{
+public:
+	int function19();
+	int pop(); //удаление элемента очереди
+	void push(int el); //добавление элемента очереди
+	void print(); //вывод на консоль очереди
+	void merge(sonQueue_private& Q1, sonQueue_private& Q2); //слияние двух очередей
+	void copy(sonQueue_private& Q); //копирование очереди
+	bool isEmpty(); //проверка на заполненность
+};
+
+class sonQueue_protected : protected parQueue
+{
+public:
+	int function19();
+	int pop(); //удаление элемента очереди
+	void push(int el); //добавление элемента очереди
+	void print(); //вывод на консоль очереди
+	void merge(sonQueue_protected& Q1, sonQueue_protected& Q2); //слияние двух очередей
+	void copy(sonQueue_protected& Q); //копирование очереди
+	bool isEmpty(); //проверка на заполненность
+};
+
+class sonQueue_public : public parQueue
+{
+public:
+	int function19();
+};
